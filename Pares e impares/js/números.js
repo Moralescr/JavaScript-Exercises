76a6993e1d7b454479e308d9bc4fcf3326210647
Adrian Morales Guadamuz <moralesguadamuz@gmail.com>
2021-03-16 20:39:03 -0600

    pares e impares

---
+++ Pares e impares/js/números.js
@@ -0,0 +1,11 @@
+
+let number = prompt('Introduce un número');
+
+for(i = 0; i <= num; num++){
+
+    if(num % 2 === 0){
+        console .log(`El número ${num} es par`);
+    } else{
+        console.log(`El número ${num} es impar`);
+    }
+}
